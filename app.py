0279cd2cc062b11e7938d3259b3e71ae41200513
vishu <vishur124@gmail.com>
2024-07-15 22:46:28 +0530

    ADD ITS

--- app.py
+++ app.py
@@ -1,28 +1,32 @@
 from flask import Flask, render_template, request, redirect, url_for
 import pickle
 import numpy as np
-import random  # Import the random module
-import os
 
 app = Flask(__name__, template_folder='template')
-port = int(os.environ.get('PORT', 5000))
-    
 
+# Load the models
 model1 = pickle.load(open('model1.pkl', 'rb'))
 model2 = pickle.load(open('model2.pkl', 'rb'))
+model3 = pickle.load(open('model3.pkl', 'rb'))
+model4 = pickle.load(open('model4.pkl', 'rb'))
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         selected_model = request.form['model_select']
         if selected_model == 'model1':
-            return redirect(url_for('input_page', model='model1'))
+            return redirect(url_for('input_page1', model='model1'))
         elif selected_model == 'model2':
-            return redirect(url_for('input_page', model='model2'))
+            return redirect(url_for('input_page1', model='model2'))
+        elif selected_model == 'model3' and 'model4':
+            return redirect(url_for('input_page', model='model3_model4'))
+        
     return render_template('index.html')
 
-@app.route('/input_page/<model>', methods=['GET', 'POST'])
-def input_page(model):
+@app.route('/input_page1/<model>', methods=['GET', 'POST'])
+def input_page1(model):
+    pred1, pred2 = None, None
+    
     if request.method == 'POST':
         aggregate = request.form['Aggregate']
         source = request.form['source']
@@ -33,32 +37,37 @@ def input_page(model):
         # Convert input data to a format that the model expects
         input_data = np.array([[aggregate, source, viscosity, dag, air_voids]])
         
-        # Initialize prediction variable
-        pred = None
-        
         # Make prediction using the selected model
         if model == 'model1':
-            pred = model1.predict(input_data)
+            pred1 = model1.predict(input_data)
+            pred1 = [f"{float(pred1)-2:.2f}", f"{float(pred1)+2:.2f}"]
         elif model == 'model2':
-            pred = model2.predict(input_data)
-        else:
-            pred = "Invalid model selection"
-
+            pred2 = model2.predict(input_data)
+            pred2 = [f"{float(pred2)-2:.2f}", f"{float(pred2)+2:.2f}"]
         
-        # Ensure prediction is extracted correctly
-        if isinstance(pred, np.ndarray):
-            pred = pred[0]
+    return render_template('input_page1.html', model=model, pred1=pred1, pred2=pred2)
 
-        # Modify the prediction value within a range of ±5 if it is numeric
-        if isinstance(pred, (int, float)):  # Ensure the prediction is a numeric type
-            formatted_pred = f"{pred:.3f}"
-            pred = formatted_pred
-            pred = float(pred)
-            # formatted_pred = f"{pred:.3f}"
+@app.route('/input_page/<model>', methods=['GET', 'POST'])
+def input_page(model):
+    pred3, pred4, tsr = None, None, None
+    
+    if request.method == 'POST':
+        aggregate = request.form['Aggregate']
+        source = request.form['source']
+        viscosity = float(request.form['viscosity'])  # Assuming viscosity is a float
+        dag = request.form['DAG']
+        air_voids = request.form['air_voids']
+        
+        # Convert input data to a format that the model expects
+        input_data = np.array([[aggregate, source, viscosity, dag, air_voids]])
+        
+        # Make prediction using the selected models
+        if model == 'model3_model4':
+            pred3 = np.round(model3.predict(input_data), 2)
+            pred4 = np.round(model4.predict(input_data), 2)
+            tsr = np.round(pred4 * 100 / pred3, 2) if pred3.all() != 0 else "Invalid prediction: Division by zero"
 
-
-        return render_template('result.html', pred=pred)
-    return render_template('input_page.html', model=model)
+    return render_template('input_page.html', model=model, pred3=pred3, pred4=pred4, tsr=tsr)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=port,debug=True)
+    app.run(debug=True)
